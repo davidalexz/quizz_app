8cc9fa8aaf12fc432c2596ddba27062f34d9dbdc
Alex David <alexz.chimi@gmail.com>
2023-11-26 21:23:27 +0200

    added style

---
+++ script.js
@@ -0,0 +1,74 @@
+const harryPotterQuestions = [
+    {
+        question: "What is the name of Harry Potter's owl?",
+        correct: 'Hedwig',
+        a: 'Crookshanks',
+        b: 'Pigwidgeon',
+        c: 'Errol',
+    },
+    {
+        question: 'Who is the Half-Blood Prince?',
+        correct: 'Severus Snape',
+        a: 'Remus Lupin',
+        b: 'Sirius Black',
+        c: 'Peter Pettigrew',
+    },
+    {
+        question: 'What is the name of the Weasley twins?',
+        correct: 'Fred and George',
+        a: 'Charlie and Bill',
+        b: 'Percy and Ron',
+        c: 'Arthur and Fred',
+    },
+    {
+        question: 'Which house did the Sorting Hat originally consider putting Harry in?',
+        correct: 'Slytherin',
+        a: 'Hufflepuff',
+        b: 'Ravenclaw',
+        c: 'Gryffindor',
+    },
+    {
+        question: 'Who is the headmaster of Hogwarts throughout most of the series?',
+        correct: 'Albus Dumbledore',
+        a: 'Minerva McGonagall',
+        b: 'Severus Snape',
+        c: 'Remus Lupin',
+    },
+    {
+        question: "What is the form of Hermione Granger's Patronus?",
+        correct: 'An otter',
+        a: 'A cat',
+        b: 'A bird',
+        c: 'A dog',
+    },
+    {
+        question: 'What magical object allows someone to transform into another person?',
+        correct: 'Polyjuice Potion',
+        a: 'Felix Felicis',
+        b: 'Veritaserum',
+        c: 'Amortentia',
+    },
+    {
+        question: 'What are the three kinds of balls used in Quidditch?',
+        correct: 'Quaffle, Bludgers, Golden Snitch',
+        a: 'Quaffle, Beaters, Golden Snitch',
+        b: 'Bludgers, Snitch, Quaffle',
+        c: 'Quaffle, Beaters, Bludgers',
+    },
+    {
+        question: "Who is the author of 'Fantastic Beasts and Where to Find Them'?",
+        correct: 'Newt Scamander',
+        a: 'Gilderoy Lockhart',
+        b: 'Rubeus Hagrid',
+        c: 'Newton Artemis Fido Scamander',
+    },
+    {
+        question:
+            "What is the name of the village where Harry's parents lived and where he defeated Voldemort?",
+        correct: "Godric's Hollow",
+        a: 'Hogsmeade',
+        b: 'Diagon Alley',
+        c: 'Little Whinging',
+    },
+    // ... more questions
+];
